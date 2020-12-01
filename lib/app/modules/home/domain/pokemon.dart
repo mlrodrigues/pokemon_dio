0eda74e0931ff91fc23372978fdd0ed2fc14dcec
mlrodrigues <marialuisaoliveirarodrigues@gmail.com>
2020-11-30 21:45:49 -0300

    Criação da lista

---
+++ lib/app/modules/home/domain/pokemon.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+class Pokemon{
+  final String id;
+  final List<dynamic> types;
+  final String imageUrl;
+  final String imageUrlHiRes;
+  final String name;
+
+  Pokemon({
+    @required this.id,
+    @required this.types,
+    @required this.imageUrl,
+    @required this.imageUrlHiRes,
+    @required this.name
+  });
+
+
+}
